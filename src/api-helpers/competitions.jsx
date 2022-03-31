6bfe5c613a140d2a5ee3064962b2032cc9342cf8
vickyruud <aprajit.k@gmail.com>
2022-03-30 20:39:37 -0400

    move function to helper file

--- src/api-helpers/competitions.jsx
+++ src/api-helpers/competitions.jsx
@@ -1,8 +1,17 @@
+import axios from 'axios';
+ 
+
+let config = {
+    headers: {
+      'X-Auth-token' : '9385d8443cff45b496b5ea43a1abf799'
+    }
+  }
+
 //function to get competition data
 export const getCompetitions = () => {
-  axios.get('http://api.football-data.org/v2/competitions/2021', config).then(res => {
-    return res
+  return fetch('http://api.football-data.org/v2/competitions/2021', config).then(res => {
+    return res.json()
   }).then(response => {
-    return response.data.seasons
+    return response
   })
 };
--- src/components/GetData.jsx
+++ src/components/GetData.jsx
@@ -1,34 +1,34 @@
 import React, { useEffect } from 'react';
-import axios from 'axios';
+import { getCompetitions } from '../api-helpers/competitions';
 
 
 const GetData = () => {
-  const [data, setData] = React.useState([]);
+  const [data, setData] = React.useState();
   const [error, setError] = React.useState(false);
   const [loading, setLoading] = React.useState(false);
 
-  let config = {
-    headers: {
-      'X-Auth-token' : '9385d8443cff45b496b5ea43a1abf799'
-    }
-  }
+ 
   
-  let urlData = {}
+  
 
   useEffect(() => {
-   
+    getCompetitions().then(response => {
+      setData(response);
+      setLoading(true);
+      console.log(data);
+    }).catch(error => {
+      setError(true);
+   })
     
   }, [])
   
-
-  const arrayData = data.map((item, i) => {
-      return (
-        <div>{item}</div>
-      )
-    })
+  
+  
 
   return (
-    <div>{arrayData }</div>
+    <div>
+      {data ? <div>Hello</div> : <div>Error loading data</div>}
+    </div>
   )
 }
 
